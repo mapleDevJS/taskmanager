808a4fd6673aa663365cb3896dac93b1af00768e
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-04 22:07:24 -0400

    Created render tasks function

--- src/components/board.js
+++ src/components/board.js
@@ -1,5 +1,11 @@
+import {renderTasks} from "./task.js";
+
+const QUANTITY_TASKS = 8;
+
 export const createBoardTemplate = () => {
   return `<section class="board container">
-    <div class="board__tasks"></div>
+    <div class="board__tasks">
+      ${renderTasks(QUANTITY_TASKS)}
+    </div>
   </section>`;
 };
