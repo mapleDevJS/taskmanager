39fa9bd9024bc8b5e646acb78c69b0ac255a44b3
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-11 11:07:38 -0400

    Created consts

---
+++ src/consts.js
@@ -0,0 +1,49 @@
+export const QuantityTasks = {
+  TOTAL: 15,
+  ON_START: 8,
+  TASKS_BY_BUTTON: 8
+};
+
+export const FILTER_NAMES = [
+  `all`,
+  `overdue`,
+  `today`,
+  `favorites`,
+  `repeating`,
+  `archive`
+];
+
+export const MONTH_NAMES = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+export const DEFAULT_REPEATING_DAYS = {
+  "mo": false,
+  "tu": false,
+  "we": false,
+  "th": false,
+  "fr": false,
+  "sa": false,
+  "su": false
+};
+
+export const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+export const DESCRIPTION_ITEMS = [
+  `Изучить теорию`,
+  `Сделать домашку`,
+  `Пройти интенсив на соточку`
+];
