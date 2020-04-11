8a176cc74dfd495a9f9e0fadd73da7ab49eab02b
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-11 11:17:51 -0400

    Fixed main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,16 @@
-import {createSiteMenuTemplate} from "./components/site-menu.js";
-import {createSortingTemplate} from "./components/sorting";
-import {createFilterTemplate} from "./components/filter.js";
-import {createLoadMoreButtonTemplate} from "./components/load-more-button.js";
-import {createBoardTemplate} from "./components/board.js";
+import {createSiteMenuTemplate} from "./components/site-menu";
+import {createFilterTemplate} from "./components/filter";
+import {createBoardTemplate} from "./components/board";
+import {addListeners} from "./components/load-more-button";
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const render = (container, template, place = `beforeend`) => {
+export const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
 render(siteHeaderElement, createSiteMenuTemplate());
 render(siteMainElement, createFilterTemplate());
 render(siteMainElement, createBoardTemplate());
-
-const siteBoardElement = siteMainElement.querySelector(`.board`);
-render(siteBoardElement, createSortingTemplate(), `afterbegin`);
-render(siteBoardElement, createLoadMoreButtonTemplate());
+addListeners();
