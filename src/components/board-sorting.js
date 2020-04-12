d8cb04ab4701ae67b7f1fd1b5a6dd4321dab6a8e
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-11 21:46:57 -0400

    Changed name & refactored module

--- src/components/board-sorting.js
+++ src/components/board-sorting.js
@@ -1,13 +1,7 @@
-const renderSort = () => {
-  return createSortingTemplate();
-};
-
-const createSortingTemplate = () => {
+export const createSortingTemplate = () => {
   return `<div class="board__filter-list">
     <a href="#" class="board__filter">SORT BY DEFAULT</a>
     <a href="#" class="board__filter">SORT BY DATE up</a>
     <a href="#" class="board__filter">SORT BY DATE down</a>
   </div>`;
 };
-
-export {renderSort};
