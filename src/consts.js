bf7c7590aaf947204686f98605d4f823308001d7
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-12 11:16:04 -0400

    Refactored consts

--- src/consts.js
+++ src/consts.js
@@ -33,8 +33,8 @@ export const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
 
 export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
 
-export const DESCRIPTION_ITEMS = [
-  `Изучить теорию`,
-  `Сделать домашку`,
-  `Пройти интенсив на соточку`
-];
+// export const DESCRIPTION_ITEMS = [
+//   `Изучить теорию`,
+//   `Сделать домашку`,
+//   `Пройти интенсив на соточку`
+// ];
