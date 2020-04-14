36ae2d8fe1b1ac4d77496f41252c2dc93c09e4fc
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-13 22:51:44 -0400

    Deleted comments in board

--- src/components/board.js
+++ src/components/board.js
@@ -1,15 +1,5 @@
 import {createSortingTemplate} from "./board-sorting";
-// import {createTaskTemplate} from "./task";
 import {createLoadMoreButtonTemplate} from "./load-more-button.js";
-// import {tasks} from "../main";
-
-// export const renderTasks = (start, end) => {
-//   let markup = ``;
-//   for (let i = start; i < end; i++) {
-//     markup += createTaskTemplate(tasks[i]);
-//   }
-//   return markup;
-// };
 
 export const createBoardTemplate = () => {
   return `<section class="board container">
