12159e7e749b7a54fbb6bc1ed9b0be0d3fb52a05
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-19 19:20:48 -0400

    Refactored TaskEdit

--- src/components/task/task-edit.js
+++ src/components/task/task-edit.js
@@ -1,108 +1,120 @@
 import {COLORS, DAYS, MONTH_NAMES} from "../../util/consts";
 import {createElement, formatTime} from "../../util/util";
-import {createColorsMarkup} from "./task-color";
-import {createRepeatingDaysMarkup} from "./repeating-days";
-
-const toggleYesNo = (element) => {
-  return element ? `yes` : `no`;
-};
-
-const showDeadline = (isDateShowing, date, time) => {
-  return isDateShowing ?
-    `<fieldset class="card__date-deadline">
-                  <label class="card__input-deadline-wrap">
-                    <input
-                      class="card__date"
-                      type="text"
-                      placeholder=""
-                      name="date"
-                      value="${date} ${time}"
-                    />
-                  </label>
-                </fieldset>`
-    : ``;
-};
-
-const getRepeatingTaskMarkup = (isRepeatingTask, repeatingDaysMarkup) => {
-  return isRepeatingTask ?
-    `<fieldset class="card__repeat-days">
-        <div class="card__repeat-days-inner">
-          ${repeatingDaysMarkup}
-        </div>
-      </fieldset>`
-    : ``;
-};
-
-export const createTaskEditTemplate = ({description, dueDate, color, repeatingDays}) => {
-  const isExpired = dueDate instanceof Date && dueDate < Date.now();
-  const isDateShowing = !!dueDate;
-  const date = isDateShowing ? `${dueDate.getDate()} ${MONTH_NAMES[dueDate.getMonth()]}` : ``;
-  const time = isDateShowing ? formatTime(dueDate) : ``;
-  const isRepeatingTask = Object.values(repeatingDays).some(Boolean);
-  const repeatClass = isRepeatingTask ? `card--repeat` : ``;
-  const deadlineClass = isExpired ? `card--deadline` : ``;
-  const repeatingDaysMarkup = createRepeatingDaysMarkup(DAYS, repeatingDays);
-
-  return (
-    `<article class="card card--edit card--${color} ${repeatClass} ${deadlineClass}">
-      <form class="card__form" method="get">
-        <div class="card__inner">
-          <div class="card__color-bar">
-            <svg class="card__color-bar-wave" width="100%" height="10">
-              <use xlink:href="#wave"></use>
-            </svg>
-          </div>
-
-          <div class="card__textarea-wrap">
-            <label>
-              <textarea
-                class="card__text"
-                placeholder="Start typing your text here..."
-                name="text"
-              >${description}</textarea>
-            </label>
-          </div>
-
-          <div class="card__settings">
-            <div class="card__details">
-              <div class="card__dates">
-                <button class="card__date-deadline-toggle" type="button">
-                  date: <span class="card__date-status">${toggleYesNo(isDateShowing)}</span>
-                </button>
-                ${showDeadline(isDateShowing, date, time)}
-                <button class="card__repeat-toggle" type="button">
-                  repeat:<span class="card__repeat-status">${toggleYesNo(isRepeatingTask)}</span>
-                </button>
-                ${getRepeatingTaskMarkup(isRepeatingTask, repeatingDaysMarkup)}
-              </div>
-            </div>
-
-            <div class="card__colors-inner">
-              <h3 class="card__colors-title">Color</h3>
-              <div class="card__colors-wrap">
-              ${createColorsMarkup(COLORS, color)}
-              </div>
-            </div>
-          </div>
-
-          <div class="card__status-btns">
-            <button class="card__save" type="submit">save</button>
-            <button class="card__delete" type="button">delete</button>
-          </div>
-        </div>
-      </form>
-    </article>`
-  );
-};
+import RepeatingDay from "./repeating-days";
+import Color from "./task-color";
 
 export default class TaskEdit {
   constructor(task) {
     this._task = task;
+    this._color = task.color;
+    this._description = task.description;
+    this._isDateShowing = !!task.dueDate;
+    this._repeatingDays = task.repeatingDays;
+    this._isRepeatingTask = Object.values(task.repeatingDays).some(Boolean);
     this._element = null;
   }
 
+  _toggleYesNo(element) {
+    return element ? `yes` : `no`;
+  }
+
+  _showDeadline() {
+    return this._isDateShowing ?
+      `<fieldset class="card__date-deadline">
+                    <label class="card__input-deadline-wrap">
+                      <input
+                        class="card__date"
+                        type="text"
+                        placeholder=""
+                        name="date"
+                        value="${this._getDate()} ${this._getTime()}"
+                      />
+                    </label>
+                  </fieldset>`
+      : ``;
+  }
+
+  _getRepeatingTaskMarkup(repeatingDaysMarkup) {
+    return this._isRepeatingTask ?
+      `<fieldset class="card__repeat-days">
+          <div class="card__repeat-days-inner">
+            ${repeatingDaysMarkup}
+          </div>
+        </fieldset>`
+      : ``;
+  }
+
+  _getDate() {
+    return this._isDateShowing ? `${this._task.dueDate.getDate()} ${MONTH_NAMES[this._task.dueDate.getMonth()]}` : ``;
+  }
+
+  _getTime() {
+    return this._isDateShowing ? formatTime(this._task.dueDate) : ``;
+  }
+
+  _getDeadlineClass() {
+    return this._task.dueDate instanceof Date && this._task.dueDate < Date.now() ? `card--deadline` : ``;
+  }
+
+  _getRepeatClass() {
+    return this._isRepeatingTask ? `card--repeat` : ``;
+  }
+
+  _getRepeatingDaysMarkup() {
+    return new RepeatingDay(DAYS, this._repeatingDays).getTemplate();
+  }
+
   getTemplate() {
-    return createTaskEditTemplate(this._task);
+    return (
+      `<article class="card card--edit card--${this._color} ${this._getRepeatClass()} ${this._getDeadlineClass()}">
+        <form class="card__form" method="get">
+          <div class="card__inner">
+            <div class="card__color-bar">
+              <svg class="card__color-bar-wave" width="100%" height="10">
+                <use xlink:href="#wave"></use>
+              </svg>
+            </div>
+
+            <div class="card__textarea-wrap">
+              <label>
+                <textarea
+                  class="card__text"
+                  placeholder="Start typing your text here..."
+                  name="text"
+                >${this._description}</textarea>
+              </label>
+            </div>
+
+            <div class="card__settings">
+              <div class="card__details">
+                <div class="card__dates">
+                  <button class="card__date-deadline-toggle" type="button">
+                    date: <span class="card__date-status">${this._toggleYesNo(this._isDateShowing)}</span>
+                  </button>
+                  ${this._showDeadline()}
+                  <button class="card__repeat-toggle" type="button">
+                    repeat:<span class="card__repeat-status">${this._toggleYesNo(this._isRepeatingTask)}</span>
+                  </button>
+                  ${this._getRepeatingTaskMarkup(this._isRepeatingTask, this._getRepeatingDaysMarkup())}
+                </div>
+              </div>
+
+              <div class="card__colors-inner">
+                <h3 class="card__colors-title">Color</h3>
+                <div class="card__colors-wrap">
+                ${new Color(COLORS, this._color).getTemplate()}
+                </div>
+              </div>
+            </div>
+
+            <div class="card__status-btns">
+              <button class="card__save" type="submit">save</button>
+              <button class="card__delete" type="button">delete</button>
+            </div>
+          </div>
+        </form>
+      </article>`
+    );
   }
 
   getElement() {
