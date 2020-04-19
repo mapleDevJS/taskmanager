696f56826c4aaa4feebd8af05158553f94ffbaf4
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-19 19:17:12 -0400

    Refactored Sorting

--- src/components/board/sorting.js
+++ src/components/board/sorting.js
@@ -6,27 +6,23 @@ const SortTypes = [
   `DATE down`
 ];
 
-const createSortTemplate = () => {
-  let markup = ``;
-  for (const type of SortTypes) {
-    markup += `<a href="#" class="board__filter">SORT BY ${type}</a>`;
-  }
-  return markup;
-};
-
-export const createSortingTemplate = () => {
-  return `<div class="board__filter-list">
-    ${createSortTemplate()}
-  </div>`;
-};
-
 export default class Sort {
   constructor() {
     this._element = null;
   }
 
+  _getSortTemplate() {
+    let markup = ``;
+    for (const type of SortTypes) {
+      markup += `<a href="#" class="board__filter">SORT BY ${type}</a>`;
+    }
+    return markup;
+  }
+
   getTemplate() {
-    return createSortTemplate();
+    return `<div class="board__filter-list">
+    ${this._getSortTemplate()}
+    </div>`;
   }
 
   getElement() {
