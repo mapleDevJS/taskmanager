495dbea5d333e3aa76ce48fcbe55feea2b413b7f
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-18 21:55:50 -0400

    Fixed imports at entry point

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import {createSiteMenuTemplate} from "./components/menu/site-menu";
-import {createFilterTemplate} from "./components/filter/filter";
-import {createBoardTemplate} from "./components/board/board";
-import {createTaskTemplate} from "./components/task/task";
-import {createTaskEditTemplate} from "./components/task/task-edit";
-// import {addListenerOnLoadMoreButton} from "./components/board/load-more-button";
+import BoardComponent from "./components/board/board";
+import FilterComponent from "./components/filter/filter";
+import TaskEditComponent from "./components/task/task-edit";
+import TaskComponent from "./components/task/task";
+import TasksComponent from "./components/task/tasks";
+import SiteMenuComponent from "./components/menu/site-menu";
+import SortComponent from "./components/board/sorting";
 import {QuantityTasks} from "./util/consts";
 import {generateTasks} from "./mocks/tasks";
 import {render} from "./util/util";
+
 
 let showingTasksCount = QuantityTasks.ON_START;
 const siteMainElement = document.querySelector(`.main`);
@@ -16,17 +18,18 @@ const renderTasks = (container, template, start, end) => {
   tasks.slice(start, end).forEach((task) => render(container, template(task)));
 };
 
-// const loadMoreClickHandler = () => {
-//   const prevTasksCount = showingTasksCount;
-//   showingTasksCount += QuantityTasks.BY_BUTTON;
+const loadMoreClickHandler = () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount += QuantityTasks.BY_BUTTON;
 
-//   if (showingTasksCount > tasks.length) {
-//     renderTasks(boardTasksElement, createTaskTemplate, prevTasksCount, tasks.length);
-//     document.querySelector(`.load-more`).remove();
-//   } else {
-//     renderTasks(boardTasksElement, createTaskTemplate, prevTasksCount, showingTasksCount);
-//   }
-// };
+  if (showingTasksCount > tasks.length) {
+    renderTasks(boardTasksElement, createTaskTemplate, prevTasksCount, tasks.length);
+    document.querySelector(`.load-more`).remove();
+  } else {
+    renderTasks(boardTasksElement, createTaskTemplate, prevTasksCount, showingTasksCount);
+  }
+};
+
 const tasks = generateTasks(QuantityTasks.TOTAL);
 
 render(siteHeaderElement, createSiteMenuTemplate());
