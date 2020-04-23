2846cf1a0c78b872d983662d18c6f71112a03c09
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-23 13:32:07 -0400

    Add function createElements

--- src/util/dom-util.js
+++ src/util/dom-util.js
@@ -1,6 +1,22 @@
-export const createElement = (template) => {
+const createElement = (template) => {
   const newElement = document.createElement(`div`);
   newElement.innerHTML = template;
 
   return newElement.firstChild;
 };
+
+const createElements = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement;
+};
+
+const toMarkup = (element) => {
+  const temp = document.createElement(`div`);
+  temp.append(element);
+  console.log(temp.innerHTML);
+  return temp.innerHTML;
+};
+
+export {createElement, createElements, toMarkup};
