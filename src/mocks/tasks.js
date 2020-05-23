25433b0a4736e6a8d2cc16c903b06c4f4218de3c
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-05-22 23:10:38 -0400

    Add model Task as adapter to server data

--- src/mocks/tasks.js
+++ src/mocks/tasks.js
@@ -1,36 +1,36 @@
-import {COLORS, DAYS} from "../util/consts";
-import {getRandomBoolean, getRandomDate, getRandomItem} from "../util/util";
+// import {COLORS, DAYS} from "../util/consts";
+// import {getRandomBoolean, getRandomDate, getRandomItem} from "../util/util";
 
-const TASK_DESCRIPTIONS = [
-  `Изучить теорию`,
-  `Сделать домашку`,
-  `Пройти интенсив на соточку`
-];
+// const TASK_DESCRIPTIONS = [
+//   `Изучить теорию`,
+//   `Сделать домашку`,
+//   `Пройти интенсив на соточку`
+// ];
 
-const getRandomRepeating = (dueDate) => {
-  const repeatingDays = {};
+// const getRandomRepeating = (dueDate) => {
+//   const repeatingDays = {};
 
-  DAYS.map((day) => {
-    repeatingDays[day] = (dueDate === null) ? getRandomBoolean() : false;
-  });
+//   DAYS.map((day) => {
+//     repeatingDays[day] = (dueDate === null) ? getRandomBoolean() : false;
+//   });
 
-  return repeatingDays;
-};
+//   return repeatingDays;
+// };
 
-const generateTask = () => {
-  const dueDate = getRandomBoolean() ? null : getRandomDate();
+// const generateTask = () => {
+//   const dueDate = getRandomBoolean() ? null : getRandomDate();
 
-  return {
-    id: String(new Date() + Math.random()),
-    description: getRandomItem(TASK_DESCRIPTIONS),
-    dueDate,
-    repeatingDays: getRandomRepeating(dueDate),
-    color: getRandomItem(COLORS),
-    isArchive: getRandomBoolean(),
-    isFavorite: getRandomBoolean(),
-  };
-};
+//   return {
+//     id: String(new Date() + Math.random()),
+//     description: getRandomItem(TASK_DESCRIPTIONS),
+//     dueDate,
+//     repeatingDays: getRandomRepeating(dueDate),
+//     color: getRandomItem(COLORS),
+//     isArchive: getRandomBoolean(),
+//     isFavorite: getRandomBoolean(),
+//   };
+// };
 
-export const generateTasks = (count) => {
-  return new Array(count).fill(``).map(generateTask);
-};
+// export const generateTasks = (count) => {
+//   return new Array(count).fill(``).map(generateTask);
+// };
